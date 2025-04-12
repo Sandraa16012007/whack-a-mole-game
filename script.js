6d8ede23c5ee24204e3b40898a6e862af8c15f25
Sandraa16012007 <sandrarosaprince@gmail.com>
2025-04-12 23:00:55 +0530

    game logic added

--- script.js
+++ script.js
@@ -1,4 +1,65 @@
 const cursor = document.querySelector('.cursor');
+const holes = [...document.querySelectorAll('.hole')];
+
+const scoreEl = document.querySelector('.score span');
+let score = 0;
+
+const sound = new Audio("whack.mp3");
+
+function run(){
+    const i = Math.floor(Math.random() * holes.length)
+    const hole = holes[i];
+    const img = document.createElement('img');
+    img.classList.add('mole');
+    img.src = 'mole.png';
+    img.style.borderBottomLeftRadius = '30%';
+    img.style.borderBottomRightRadius = '30%';
+
+    img.addEventListener('click', () => {
+        score += 10;
+        sound.play();
+        scoreEl.textContent = score;
+        img.src = 'mole-whacked.png';
+        clearTimeout(timer);
+        setTimeout(() => {
+            hole.removeChild(img)
+            run()
+        }, 500);
+    })
+
+    hole.appendChild(img);
+
+    timer = setTimeout(() => {
+        hole.removeChild(img)
+        run()
+    }, 1500);
+
+}
+
+const startButton = document.getElementById('start-button');
+startButton.addEventListener('click', () => {
+    score = 0;
+    scoreEl.textContent = score;
+    holes.forEach(hole => {
+        const mole = hole.querySelector('.mole');
+        if (mole) {
+            hole.removeChild(mole);
+        }
+    });
+    run();
+});
+const pauseButton = document.getElementById('pause-button');
+pauseButton.addEventListener('click', () => {
+    holes.forEach(hole => {
+        const mole = hole.querySelector('.mole');
+        if (mole) {
+            hole.removeChild(mole);
+        }
+    });
+    clearTimeout(timer);
+});
+
+
 window.addEventListener( "mousemove", e=>{
     cursor.style.top = e.pageY + "px";  
     cursor.style.left = e.pageX + "px";
